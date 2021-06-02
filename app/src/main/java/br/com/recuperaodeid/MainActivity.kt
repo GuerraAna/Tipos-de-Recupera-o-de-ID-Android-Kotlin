08b0ff2e551bbc2d1cca2d0eb33cfca01ee8f589
Ana Clara Guerra <ana.guerrads@hotmail.com>
2021-06-02 00:30:14 -0300

    ViewBinding

--- app/src/main/java/br/com/recuperaodeid/MainActivity.kt
+++ app/src/main/java/br/com/recuperaodeid/MainActivity.kt
@@ -2,10 +2,22 @@ package br.com.recuperaodeid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
+import br.com.recuperaodeid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btEnter.setOnClickListener {
+            Toast.makeText(this, "Aqui tem um Toast", Toast.LENGTH_SHORT).show()
+        }
     }
+
 }
