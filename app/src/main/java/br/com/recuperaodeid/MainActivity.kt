15935821f3fc3d88e3c7db6ce759efc205e75415
Ana Clara Guerra <ana.guerrads@hotmail.com>
2021-06-02 00:42:18 -0300

    FindViewByID

--- app/src/main/java/br/com/recuperaodeid/MainActivity.kt
+++ app/src/main/java/br/com/recuperaodeid/MainActivity.kt
@@ -2,15 +2,16 @@ package br.com.recuperaodeid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        bt_enter.setOnClickListener {
+        val enterButton = findViewById<Button>(R.id.bt_enter)
+        enterButton.setOnClickListener {
             Toast.makeText(this, "Aqui tem um Toast", Toast.LENGTH_SHORT).show()
         }
     }
